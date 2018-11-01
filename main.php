f2e1a57f52da3b5cab09b919394547bd2d17702b
DrenKelmendi <drenkelmendi@hotmail.com>
2018-11-01 16:06:47 +0100

    Kam shtuar foto dhe kam ndryshuar headerin

---
+++ main.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+<link rel="stylesheet" type="text/css"  href="Detyra/style1.css" />
+
+</head>
+
+<body>
+
+	<div id="header" class="group"><div id="header-inner">
+	<div id="nav" class="group">
+		<div id="logo" class="group">
+			
+				
+		</div>
+
+	
+
+		<ul class="group">
+			<li><a href="#"><strong>Home </strong></a></li>
+			<li><a href="#"><strong>Home </strong></a></li>
+			<li><a href="#"><strong>Home </strong></a></li>
+			<li class="last"><a href="#"><strong>Home <br></br></strong></a></li>
+		</ul>
+	</div>
+
+
+
+
+</body>
+</html>
