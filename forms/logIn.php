a618dc559c29205976706371d4600afa68f0002b
Drenkelmendi <drenkelmendii@hotmail.com>
2018-12-03 11:00:58 +0100

    midifikimi i marginave te format (logIn, signUp)

--- forms/logIn.php
+++ forms/logIn.php
@@ -1,15 +1,18 @@
 	<form  method="post" action="func/logInCheck.php">
-		<div class="form-group">
+		
+		<div class="form-group col-md-5">
 			<label>Username: </label>
 			<input name ="username" type="text" class="form-control" required autofocus placeholder="Username">
 		</div>
 
-		<div class="form-group">
+		<div class="form-group col-md-5">
 			<label>Password: </label>
 			<input name="password" type="password" class="form-control" required  placeholder="Password">
 		</div>
-
+		
 		<button type="submit" name="logCheck" class="btn btn-primary">Log In</button>
+
+
 	</form>
 
 	 
--- forms/signUp.php
+++ forms/signUp.php
@@ -1,36 +1,36 @@
 
 	<form  method="post" action="func/addUser.php">
-		<div class="form-group">
+		<div class="form-group col-md-5">
 			<label>Username: </label>
 			<input type="text" name="uname" class="form-control" required autofocus placeholder="Username">
 		</div>
 
-		<div class="form-group">
+		<div class="form-group col-md-5">
 			<label>Password: </label>
 			<input type="password" name="password" class="form-control" required  placeholder="Password">
 		</div>
 
-		<div class="form-group">
+		<div class="form-group col-md-5">
 			<label>Confirm Password: </label>
 			<input type="password" name="userCPassword" class="form-control" required  placeholder="Confirm Password">
 		</div>
 
-		<div class="form-group">
+		<div class="form-group col-md-5">
 			<label>First Name: </label>
 			<input type="text" name="fname" class="form-control" required  placeholder="First Name">
 		</div>
 
-		<div class="form-group">
+		<div class="form-group col-md-5">
 			<label>Last Name: </label>
 			<input type="text" name="lname" class="form-control" required  placeholder="Last Name">
 		</div>
 
-		<div class="form-group">
+		<div class="form-group col-md-5">
 			<label>Address: </label>
 			<input type="text" name="address" class="form-control" required  placeholder="Address">
 		</div>
 
-		<div class="form-group">
+		<div class="form-group col-md-5	">
 			<label>Qyteti: </label>
 			<input type="text" name="qyteti" class="form-control" required  placeholder="Qyteti">
 		</div>
